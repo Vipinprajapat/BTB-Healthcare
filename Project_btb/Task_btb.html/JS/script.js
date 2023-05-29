ae8cd702037b1e3dd5b650c1449a1e70a662360b
Vipin_✌️✨ <112061333+Vipinprajapat@users.noreply.github.com>
2023-05-29 14:31:56 +0530

    Add files via upload

---
+++ Project_btb/Task_btb.html/JS/script.js
@@ -0,0 +1,73 @@
+var iconArray =
+{
+
+    arrow : '<svg id="Component_145_1" data-name="Component 145 – 1" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50"><g id="Ellipse_61" data-name="Ellipse 61" fill="#fff" stroke="#ccc" stroke-width="1"><circle cx="25" cy="25" r="25" stroke="none"/><circle cx="25" cy="25" r="24.5" fill="none"/></g><g id="Group_8811" data-name="Group 8811" transform="translate(-175.086 -177.824)"><path d="M195.69,213.824c-.774-.341-.716-1.116-.213-1.651,2.853-3.034,5.68-6.1,8.516-9.149a1.59,1.59,0,0,0,.223-.255.935.935,0,0,1-.206-.13q-4.3-4.622-8.594-9.247a.953.953,0,0,1-.287-.981.758.758,0,0,1,1.229-.4,1.951,1.951,0,0,1,.161.16q4.62,4.974,9.234,9.953a1.451,1.451,0,0,1,.31.578.763.763,0,0,1-.185.693c-.053.064-.109.125-.166.185q-4.542,4.891-9.087,9.778a5.079,5.079,0,0,1-.578.468Z" transform="translate(0)" fill="#999" stroke="#999" stroke-width="1"/></g></svg>',
+
+    
+    phone : '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="512" height="512" x="0" y="0" viewBox="0 0 511.438 511.438" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path fill="#fec165" d="M488.299 65.499H23.139c-8.64 0-15.64 7-15.64 15.64v81.45l58.46 58.599h401.022l36.959-58.599v-81.45c-.001-8.64-7.001-15.64-15.641-15.64z" data-original="#fec165" class=""></path><path fill="#97da7b" d="M478.876 290.251H70.985L7.499 348.849v81.45c0 8.64 7 15.64 15.64 15.64h465.16c8.64 0 15.64-7 15.64-15.64v-81.45z" data-original="#97da7b"></path><path fill="#fdb441" d="M66.117 65.499H23.139c-8.64 0-15.64 7-15.64 15.64v81.45l58.46 58.599h.159V65.499z" data-original="#fdb441"></path><path fill="#80d261" d="M130.539 411.408c-35.579 0-64.421-28.842-64.421-64.421v-52.243L7.499 348.849v81.45c0 8.638 7.002 15.64 15.64 15.64h465.16c8.638 0 15.64-7.002 15.64-15.64v-18.892h-373.4z" data-original="#80d261"></path><path fill="#f9f7f8" d="M7.499 162.589h496.44v186.26H7.499z" data-original="#f9f7f8" class=""></path><path fill="#efedef" d="M7.499 162.589h58.618v186.26H7.499z" data-original="#efedef"></path><path d="M488.303 58.002H93.81a7.5 7.5 0 0 0 0 15h394.494c4.486 0 8.135 3.65 8.135 8.136v73.954h-64.051a7.5 7.5 0 0 0 0 15h64.051v171.255H77.828a7.5 7.5 0 0 0 0 15h418.61v73.954c0 4.486-3.649 8.136-8.135 8.136H23.135c-4.486 0-8.135-3.65-8.135-8.136v-73.954h30.866a7.5 7.5 0 0 0 0-15H15V170.091h385.425a7.5 7.5 0 0 0 0-15H15V81.138c0-4.486 3.649-8.136 8.135-8.136h38.712a7.5 7.5 0 0 0 0-15H23.135C10.378 58.002 0 68.38 0 81.138v349.163c0 12.757 10.378 23.136 23.135 23.136h465.168c12.757 0 23.135-10.378 23.135-23.136V81.138c0-12.758-10.378-23.136-23.135-23.136z" fill="#000000" data-original="#000000" class=""></path><path d="M182.101 255.719c0 40.593 33.025 73.618 73.618 73.618s73.618-33.025 73.618-73.618-33.025-73.618-73.618-73.618-73.618 33.025-73.618 73.618zm132.236 0c0 32.322-26.296 58.618-58.618 58.618s-58.618-26.296-58.618-58.618 26.296-58.618 58.618-58.618 58.618 26.296 58.618 58.618z" fill="#000000" data-original="#000000" class=""></path><path d="M248.219 217.49v10.876a28.2 28.2 0 0 0-6.531 2.715l-7.697-7.697a7.5 7.5 0 0 0-10.606 0 7.5 7.5 0 0 0 0 10.606l7.697 7.697a28.218 28.218 0 0 0-2.715 6.531H217.49a7.5 7.5 0 0 0 0 15h10.876a28.2 28.2 0 0 0 2.715 6.531l-7.697 7.697a7.5 7.5 0 0 0 5.303 12.803 7.474 7.474 0 0 0 5.303-2.197l7.697-7.697a28.218 28.218 0 0 0 6.531 2.715v10.876a7.5 7.5 0 0 0 15 0V283.07a28.2 28.2 0 0 0 6.531-2.715l7.697 7.697c1.464 1.464 3.384 2.197 5.303 2.197s3.839-.732 5.303-2.197a7.5 7.5 0 0 0 0-10.606l-7.697-7.697a28.218 28.218 0 0 0 2.715-6.531h10.876a7.5 7.5 0 0 0 0-15H283.07a28.2 28.2 0 0 0-2.715-6.531l7.697-7.697a7.5 7.5 0 0 0 0-10.606 7.5 7.5 0 0 0-10.606 0l-7.697 7.697a28.218 28.218 0 0 0-6.531-2.715V217.49a7.5 7.5 0 0 0-14.999 0zm20.867 38.229c0 7.371-5.996 13.367-13.367 13.367s-13.367-5.996-13.367-13.367 5.996-13.367 13.367-13.367 13.367 5.997 13.367 13.367z" fill="#000000" data-original="#000000" class=""></path></g></svg>',
+
+    phonecontact : '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="41 " height="41 " x="0" y="0" viewBox="0 0 512.035 512.035" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><linearGradient id="a" x1="256.017" x2="256.017" y1="512.035" y2="0" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#5558ff"></stop><stop offset="1" stop-color="#00c0ff"></stop></linearGradient><path fill="url(#a)" d="M287.011 150c41.355 0 75 33.645 75 75 0 8.284 6.716 15 15 15s15-6.716 15-15c0-57.897-47.103-105-105-105-8.284 0-15 6.716-15 15s6.716 15 15 15zm0 60c8.271 0 15 6.729 15 15 0 8.284 6.716 15 15 15s15-6.716 15-15c0-24.813-20.187-45-45-45-8.284 0-15 6.716-15 15s6.716 15 15 15zm122.69 176.858-76.367-50.911c-17.782-11.856-41.665-9.493-56.781 5.622l-24.66 24.661c-1.308.166-7.817.342-22.896-8.752-12.71-7.667-27.313-19.492-41.119-33.299-13.807-13.806-25.632-28.409-33.299-41.119-9.096-15.079-8.919-21.586-8.752-22.896l24.659-24.66c15.116-15.115 17.48-38.996 5.623-56.782l-50.912-76.367c-15.735-23.605-49.164-26.958-69.262-6.858L18.171 133.26c-17.33 17.33-31.056 51.963 1.97 118.538 21.116 42.567 57.847 91.111 103.426 136.691 45.58 45.58 94.124 82.311 136.691 103.426 38.936 19.315 87.941 32.565 118.537 1.97l37.764-37.764c20.101-20.1 16.747-53.527-6.858-69.263zm-14.355 48.049-37.764 37.764c-19.792 19.793-61.119 3.714-83.993-7.632-39.15-19.421-86.1-55.055-128.809-97.764-42.71-42.709-78.344-89.659-97.765-128.81-11.346-22.873-27.425-64.199-7.632-83.993l37.764-37.763c6.688-6.687 17.832-5.598 23.088 2.286l50.912 76.367c3.952 5.928 3.164 13.888-1.875 18.927l-25.035 25.036c-8.183 8.183-14.582 24.117 1.401 53.56 8.791 16.195 23.361 34.842 41.025 52.506 17.663 17.664 36.311 32.233 52.505 41.025 29.44 15.982 45.377 9.585 53.561 1.401l25.035-25.036c5.038-5.039 12.998-5.826 18.928-1.874l76.367 50.911c7.884 5.258 8.974 16.402 2.287 23.089zM287.011 60c-8.284 0-15 6.716-15 15s6.716 15 15 15c74.439 0 135 60.561 135 135 0 8.284 6.716 15 15 15s15-6.716 15-15c0-90.981-74.018-165-165-165zm0-60c-8.284 0-15 6.716-15 15s6.716 15 15 15c107.523 0 195 87.477 195 195 0 8.284 6.716 15 15 15s15-6.716 15-15c0-124.065-100.934-225-225-225z" data-original="url(#a)" class=""></path></g></svg>',
+    
+    
+}
+function svgIcon(e) {
+    $(e).each(function () {
+        var dataIcon = $(this).attr("data-icon");                                                                          
+        if (dataIcon in iconArray) {
+            $(this).replaceWith(iconArray[dataIcon]);
+        } else {
+            // console.log(dataIcon + ": This icon(s) does not exists")
+        }
+    });
+}
+svgIcon(".n-icon");
+/* HTML Example: <i class="n-icon" data-icon="s-x"></i> */
+
+
+//Meet Our Team
+var img1 = document.getElementById('meet_img1');
+var img2 = document.getElementById('meet_img2');
+var img3 = document.getElementById('meet_img3');
+var box_overlay = document.getElementById('overlay_img');
+
+
+function myfunc(){
+    
+}
+
+function changeImage(file) {
+    let img_ = document.querySelector('#meet_img1');
+    let img_overlay = document.querySelector("#overlay_img");
+    img_overlay.setAttribute("src",file);
+    img_overlay.style.width="325px";
+    img_overlay.style.height="auto";
+    img_overlay.style.border="8px solid white";
+    // img_overlay.style.aspectratio="4/3";
+    
+    // img_overlay.style. object-fit : "cover";
+    
+}
+// navbar
+// let bar = document.querySelector('.navbar-toggler');
+// let xmark = document.querySelector('.x-mark');
+// let menu = document.querySelector('.menu-bar');
+// let overlay = document.querySelector('.overlay');
+// const body = document.body;
+// let menuOpen = document.querySelector('.menu-open');
+// let menuClose = document.querySelector('.menu-close');
+
+// bar.addEventListener("click", ()=>{
+//     menu.classList.toggle('active');
+//     if(menu.classList.contains('active')){
+//         overlay.classList.add('active');
+//         body.classList.add('active');
+//         menuClose.style.display = 'inline-block';
+//         menuOpen.style.display = 'none';
+//     }
+//     else{
+//         overlay.classList.remove('active'); 
+//         body.classList.remove('active');    
+//         menuClose.style.display = 'none';
+//         menuOpen.style.display = 'inline-block';
+//     }
+// });
